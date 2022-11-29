5e66819c830858fb8e8b40e2e50025fd84ac4527
Andref007 <andrefps007@hotmail.com>
2022-11-29 20:57:23 -0300

    realizando a conexao do banco com o backend usando mongoose

--- config/db.js
+++ config/db.js
@@ -1,1 +1,14 @@
 const mongoose = require("mongoose")
+
+/*
+Com a importação do módulo externo do mongoose, podemos chamar a partir da variável mongoose
+o método connect() que recebe como parâmetro a string de conexão do banco, neste caso , o Mongo Atlas
+*/ 
+mongoose.connect('mongodb+srv://fs01:fs01@svl-db.lnj91aw.mongodb.net/svl')
+
+// A variável db recebe a conexão do banco e vamos exportá-la
+let db = mongoose.connection
+
+//exporta a conexão com o Mongo (Atlas ou local , dependendo da string de conexão)
+module.exports = db
+
