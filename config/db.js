effa5c183f13eee80bd64f20407672eafcdb80a9
Andref007 <andrefps007@hotmail.com>
2022-11-29 21:05:26 -0300

    conexao do back com o banco de dados

--- config/db.js
+++ config/db.js
@@ -10,5 +10,6 @@ mongoose.connect('mongodb+srv://fs01:fs01@svl-db.lnj91aw.mongodb.net/svl')
 let db = mongoose.connection
 
 //exporta a conexão com o Mongo (Atlas ou local , dependendo da string de conexão)
+//
 module.exports = db
 
